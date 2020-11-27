63b9bebcf13d53b5f4c2a2df21f9f764b33e97d4
Jus1x-by <juniorhdblr@gmail.com>
2020-11-27 14:01:51 +0300

    userPages cash

--- routes/userPages.js
+++ routes/userPages.js
@@ -1,24 +1,57 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res){
-    res.render('index');    
-});
+const Category = require('../models/category');
+const Brand = require('../models/brand');
+const Product = require('../models/product');
 
-router.get('/delivery', function(req, res){
-    res.render('delivery');
-});
-
-router.get('/payment', function(req, res){
-    res.render('payment');
+let categoryCash = Category.find( (err, categories) =>{
+    if (err) console.log(err);
+    categoryCash = categories;
 })
 
-router.get('/contacts', function(req,res){
-    res.render('contacts');
+let brandsCash = Brand.find ( (err, brands) => {
+    if (err) console.log(err);
+    brandsCash = brands;
 })
 
-router.get('/catalog', function(req,res){
-    res.render('catalog');
+router.get('/', (req, res) => {
+    res.render('index', {
+            categories: categoryCash,
+            brands: brandsCash
+        })    
+});
+
+router.get('/delivery', (req, res) => {
+    res.render('delivery', {
+            categories: categoryCash,
+            brands: brandsCash
+        })
+});
+
+router.get('/payment', (req, res) => {
+    res.render('payment', {
+        categories: categoryCash,
+        brands: brandsCash
+    })
+})
+
+router.get('/contacts', (req, res) => {
+    res.render('contacts', {
+            categories: categoryCash,
+            brands: brandsCash
+        })
+})
+
+router.get('/catalog', (req,res) => {
+    Product.find( (err, products) => {
+        if (err) console.log(err);
+        res.render('catalog', {
+            products: products,
+            categories: categoryCash,
+            brands: brandsCash
+        })
+    })
 })
 
 
