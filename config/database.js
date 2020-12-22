43d963c2511137204ff1e9c0620ff3d4bba6b432
Jus1x-by <56512587+Jus1x-by@users.noreply.github.com>
2020-12-22 03:02:48 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database : "mongodb+srv://jus1x:p2Z2YK4nMC@courseproject-cluster.0ybwi.azure.mongodb.net/courseproject-db?retryWrites=true&w=majority"
+    database : "mongodb+srv://jus1x:pass@courseproject-cluster.0ybwi.azure.mongodb.net/courseproject-db?retryWrites=true&w=majority"
 }
